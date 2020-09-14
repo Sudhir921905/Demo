3c09a143613e1b3b68e8c0bbd9ca30b1843efcb5
Sudhir921905 <71128340+Sudhir921905@users.noreply.github.com>
2020-09-14 13:22:19 +0530

    RepoDemo1 Added

---
+++ src/RepoDemo1.java
@@ -0,0 +1,13 @@
+
+public class RepoDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		demo1();
+	}
+	public static void demo1() {
+		System.out.println("Demo Repo1");
+	}
+
+}
